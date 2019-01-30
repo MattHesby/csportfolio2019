b9dbbbb8d40bacaa199b105195b30414e59c44db
Matt Hesby <mhesby@summitps.org>
2019-01-30 19:43:45 +0000

    first commit

---
+++ int2-checkpoints/circles.js
@@ -0,0 +1,12 @@
+/* global draw ellipse rect p processing width height size */
+var sketch = function(processing) {with(processing) {size(400, 400);background(255);
+// °º¤ø,¸¸,ø¤º°`°º¤ø Create Project Below This Line ø¤º°`°º¤ø,¸,ø¤°//
+
+
+draw = function() {
+    ellipse(0,0,30,30);
+    
+};    
+    
+// °º¤ø,¸¸,ø¤º°`°º¤ø Create Project Above This Line ø¤º°`°º¤ø,¸,ø¤°//   
+}};var p = new Processing(document.getElementById("output-canvas"), sketch);
